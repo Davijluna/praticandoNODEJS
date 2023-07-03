be3127d6fc19ba46d8d407676522b0d965635827
davi <davijluna@gmail.com>
2023-07-03 12:01:20 -0300

    feat: como criar rotas dinâmicas usando parâmetros

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get("/sobre", function(req, res) {
 app.get("/blog", function(req, res) {
   res.send("Bem-vindo ao meu blog!");
 });
+// quando criamos o parâmetro obrigatóriamente precisa passar o parâmetro.
+// o send só pode coloca-lo uma vez se não ele dá erro.
+app.get("/ola/:nome/:cargo/:cor", function(req, res) {
+  res.send("<h1>Olâ "+ req.params.nome + "</h1>" + "<h2>"+" você é um "+ req.params.cargo+ "</h2>"+ "<h3>" + " sua cor é "+ req.params.cor+"</h3>")
+})
 
 app.listen(8081, function() {
   console.log('Servidor Rodando na url http://localhost:8081')
