e0ec062a5594b0a3fbdce81c3187ed1a2e71139e
davi <davijluna@gmail.com>
2023-07-04 19:24:16 -0300

    feat: como inserir dados e como deletar dados especificos

--- teste.sql
+++ teste.sql
@@ -1,5 +1,22 @@
-CREATE TABLE usuarios(
-  nome VARCHAR(50),
-  email VARCHAR(100),
-  idade INT
-);
+-- CREATE TABLE usuarios(
+--   nome VARCHAR(50),
+--   email VARCHAR(100),
+--   idade INT
+-- );
+
+-- adicionando os usuarios
+-- INSERT INTO usuarios(idade, nome, email)
+-- VALUES(
+--   56,
+--   'Sandra Gusmão',
+--   "email@teste5.com"
+-- );
+
+-- lista os usuarios
+-- SELECT * FROM 
+
+-- lista um usuarios especifico 
+SELECT * FROM usuarios WHERE idade < 40;
+
+-- lista nome em string
+SELECT * FROM usuarios WHERE nome = "nome";
