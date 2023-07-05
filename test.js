9714f943f60922c1d756a728551a06cf506da422
davi <davijluna@gmail.com>
2023-07-05 15:58:01 -0300

    feat: criando usuarios

--- test.js
+++ test.js
@@ -5,8 +5,50 @@ const sequelize = new Sequelize('test', 'root', '82523614', {
   dialect: 'mysql'
 })
 
-sequelize.authenticate().then(function() {
-  console.log("Conectado com sucesso!")
-}).catch(function(erro) {
-  console.log("Falha ao se conectar:" + erro)
+// como criar um model >> ele irá criar tabelas sem precisar digitar comandos de sql.
+
+// 
+const Postagem = sequelize.define('postagens', {
+  titulo: {
+    type: Sequelize.STRING
+  },
+  conteudo: {
+    type: Sequelize.TEXT
+  }
 })
+
+// Postagem.create({
+//   titulo: "Um titulo qualquer",
+//   conteudo: "Qualquer coisa"
+// })
+
+const Usuario = sequelize.define('usuarios', {
+  nome: {
+    type: Sequelize.STRING
+  },
+  sobrenome: {
+    type: Sequelize.STRING
+  },
+  idade: {
+    type: Sequelize.INTEGER
+  },
+  email: {
+    type: Sequelize.STRING
+  }
+})
+
+Usuario.create({
+  nome: "Davi",
+  sobrenome: "Jesus",
+  idade: 34,
+  email: "email@test.com"
+})
+
+// Usuario.sync({force: true})
+
+
+// sequelize.authenticate().then(function() {
+//   console.log("Conectado com sucesso!")
+// }).catch(function(erro) {
+//   console.log("Falha ao se conectar:" + erro)
+// })
