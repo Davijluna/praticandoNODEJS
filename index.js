d2c84054de98f02acce7e1487da29f2539e8ae3e
davi <davijluna@gmail.com>
2023-07-19 23:52:55 -0300

    feat: criando a configuração para o handlebars

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express"); 
 // import express from "express";
 const app = express();
+const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
+
+// config
+  // Template Engine
+    app.engine('handlebars', handlebars({defaltLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+    // Conexão com o banco de dados MySql
+    const sequelize = new Sequelize('test', 'root', '82523614', {
+      host:"localhost",
+      dialect: 'mysql'
+    })
 
 app.listen(8081, function() {
   console.log('Servidor Rodando na url http://localhost:8081')
