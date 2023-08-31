7af13543a871d11fddbe7600ff5a964e55af3db2
davi <davijluna@gmail.com>
2023-08-31 14:37:18 -0300

    feat:adiconando model usuario para tabela

--- test.js
+++ test.js
@@ -14,6 +14,25 @@ const Postagem = sequelize.define('postagens', {
   }
 })
 
+// Criando o model de Usuario.
+
+const Usuario = sequelize.define('usuarios', {
+  nome: {
+    type: Sequelize.STRING
+  },
+  sobreNome: {
+    type: Sequelize.STRING
+  },
+  idade: {
+    type: Sequelize.INTEGER
+  },
+  email: {
+    type: Sequelize.STRING
+  }
+})
+
+
+// Usuario.sync({force: true})
 // Postagem.sync({force: true})
 
 // sequelize.authenticate().then(function() {
