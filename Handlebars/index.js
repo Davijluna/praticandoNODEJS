bba8a48e1b76f7f6e7ed73e0af355c212a31bdbe
davi <davijluna@gmail.com>
2023-09-01 14:13:20 -0300

    feat:rodando o arquivo index.js com nodemon

--- Handlebars/index.js
+++ Handlebars/index.js
@@ -4,7 +4,7 @@ const handlebars = require('express-handlebars')
 
 // config
   // Template Engine
-    app.engine('handlebars', handlebars({defaultLayout:'main'}))
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
   // Conexão com o banco de dados MySql
   // configurando a conxão com o Banco de Dados. 
@@ -13,6 +13,11 @@ const sequelize = new Sequelize('test', 'root', '82523614', {
   host: "localhost",
   dialect: 'mysql'
 })
+
+// Rotas 
+  app.get('/cad', function(req, res){
+    res.send('ROTA DE CADASTRO DE POST')
+  })
 
 app.listen(8081, function() {
   console.log('Servidor Rodando na url http://localhost:8081')
