44a37928c216aca37ca7ba9945e162975b970fce
davi <davijluna@gmail.com>
2023-09-01 01:29:33 -0300

    refactor: Organizando o código para facilitar leitura

--- Handlebars/index.js
+++ Handlebars/index.js
@@ -1,18 +1,18 @@
 const express = require("express"); 
 const app = express();
 const handlebars = require('express-handlebars')
-// configurando a conxão com o Banco de Dados. 
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('test', 'root', '82523614', {
-  host: "localhost",
-  dialect: 'mysql'
-})
-
 
 // config
   // Template Engine
     app.engine('handlebars', handlebars({defaultLayout:'main'}))
     app.set('view engine', 'handlebars')
+  // Conexão com o banco de dados MySql
+  // configurando a conxão com o Banco de Dados. 
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('test', 'root', '82523614', {
+  host: "localhost",
+  dialect: 'mysql'
+})
 
 app.listen(8081, function() {
   console.log('Servidor Rodando na url http://localhost:8081')
