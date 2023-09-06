4ec11c1f5ce9fa47a19b62515a1846912794ccae
davi <davijluna@gmail.com>
2023-09-05 23:48:00 -0300

    feat: instalando o Body Parser e colocando ele em uma const e o configurando no index.js

--- Handlebars/index.js
+++ Handlebars/index.js
@@ -1,14 +1,18 @@
 const express = require("express"); 
 const app = express();
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
 
 // config
   // Template Engine
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+  // Body Parser
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
   // Conexão com o banco de dados MySql
   // configurando a conxão com o Banco de Dados. 
-const Sequelize = require('sequelize')
 const sequelize = new Sequelize('test', 'root', '82523614', {
   host: "localhost",
   dialect: 'mysql'
