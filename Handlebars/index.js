9dee1ab1f3e4d74886a9f569f09155f2ac9f4b08
davi <davijluna@gmail.com>
2023-09-10 21:14:24 -0300

    feat: separando o sequelize em arquivos diferentes

--- Handlebars/index.js
+++ Handlebars/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-const Sequelize = require('sequelize')
+
 
 // config
   // Template Engine
@@ -11,12 +11,7 @@ const Sequelize = require('sequelize')
   // Body Parser
     app.use(bodyParser.urlencoded({extended: false}))
     app.use(bodyParser.json())
-  // Conexão com o banco de dados MySql
   // configurando a conxão com o Banco de Dados. 
-const sequelize = new Sequelize('test', 'root', '82523614', {
-  host: "localhost",
-  dialect: 'mysql'
-})
 
 // Rotas 
   app.get('/cad', function(req, res){
---
+++ Handlebars/models/db.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+
+// Conexão com o banco de dados MySql
+const sequelize = new Sequelize('test', 'root', '82523614', {
+  host: "localhost",
+  dialect: 'mysql'
+})
+
+// Exportando os dois sequelize 
+
+module.exports = {
+  Sequelize: Sequelize,
+  sequelize: sequelize
+}
