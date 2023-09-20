15e31cac1948140fe21e4de0c47156ffbd58f136
davi <davijluna@gmail.com>
2023-09-20 00:31:05 -0300

    feat: configurando o mongoose

---
+++ trabalhando_com_mongo/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+//  Configurando o mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/aprendendo", {
+  useMongoClient: true
+}).then(() => {
+  console.log('MongoDB conectado')
+}).catch((err) => {
+  console.log('Houve um erro ao se conectar ao mongoDB' + err)
+})
