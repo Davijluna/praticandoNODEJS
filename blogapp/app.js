dd67fc95bbc8ed1cbd1b73c0395c9d16190d1100
davi <davijluna@gmail.com>
2023-09-21 12:23:04 -0300

    feat: adicionando const path para arquivos de diretório public

--- blogapp/app.js
+++ blogapp/app.js
@@ -4,6 +4,7 @@
   const bodyParser = require('body-parser')
   const app = express()
   const admin = require("./routes/admin")
+  const path = require("path")
   // const mongoose = require('mongoose')
 
 // Configurações
@@ -18,6 +19,10 @@
   },
 }))
     app.set('view engine', 'handlebars');
+    // mongoose
+      // Em breve
+    // Public
+      app.use(express.static(path.join(__dirname, "public")))
 // Rotas
   app.use('/admin', admin)
 // Outros
