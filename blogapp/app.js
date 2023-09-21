ec2b23a9e8884bd3a83cdc586f1b152d935b718f
davi <davijluna@gmail.com>
2023-09-20 23:02:08 -0300

    feat: configurando o bodyParser

--- blogapp/app.js
+++ blogapp/app.js
@@ -1,6 +1,19 @@
 // Carregando módulos
-const express = require('express')
-const handlebars = require('express-handlebars')
-const bodyParser = require('body-parser')
-// const mongoose = require('mongoose')
+  const express = require('express')
+  const handlebars = require('express-handlebars')
+  const bodyParser = require('body-parser')
+  const app = express()
+  // const mongoose = require('mongoose')
 
+// Configurações
+  // Body Parser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+  
+// Rotas
+
+// Outros
+const PORT = 8081
+app.listen(PORT, () => {
+  console.log("Servidor rodando! ")
+})
