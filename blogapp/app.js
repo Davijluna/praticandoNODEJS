384642b75314d70bc1c362f157bce90c2601a823
davi <davijluna@gmail.com>
2023-09-20 23:36:59 -0300

    feat: adicionando grupo de rotas

--- blogapp/app.js
+++ blogapp/app.js
@@ -3,6 +3,7 @@
   const handlebars = require('express-handlebars')
   const bodyParser = require('body-parser')
   const app = express()
+  const admin = require("./routes/admin")
   // const mongoose = require('mongoose')
 
 // Configurações
@@ -18,7 +19,7 @@
 }))
     app.set('view engine', 'handlebars');
 // Rotas
-
+  app.use('/admin', admin)
 // Outros
 const PORT = 8081
 app.listen(PORT, () => {
