3efe1468269222f602d1f731ccbe952454af8aa2
davi <davijluna@gmail.com>
2023-09-29 16:47:05 -0300

    feat: utilizando as variáveis globais para success e error

--- blogapp/app.js
+++ blogapp/app.js
@@ -20,7 +20,9 @@
     app.use(flash())
   // Middleware
     app.use((req, res, next) => {
-      
+      res.locals.success_msg = req.flash("success_msg")
+      res.locals.error_msg = req.flash("error_msg")
+      next()
     })
   // Body Parser
     app.use(bodyParser.urlencoded({extended: true}))
