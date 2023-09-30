a36d814c6ec0fb4250c611d88d0d82c758fbca4f
davi <davijluna@gmail.com>
2023-09-30 17:08:26 -0300

    refactor: correção para success

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -74,7 +74,7 @@ router.post("/categorias/edit", (req, res) => {
     categoria.slug = req.body.slug
 
     categoria.save().then(() => {
-      req.flash("sucess_msg", "Categoria editada com sucesso!")
+      req.flash("success_msg", "Categoria editada com sucesso!")
       res.redirect("/admin/categorias")
     }).catch((err) => {
       req.flash("error_msg", "Houve um erro interno ao salvar a categoria")
