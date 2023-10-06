ce8f00fa634e33c17607342dc886066c987d2f86
davi <davijluna@gmail.com>
2023-10-06 18:35:14 -0300

    feat: carregando o model de postagens e exibindo em tela as postagens que foram criadas

--- blogapp/app.js
+++ blogapp/app.js
@@ -8,6 +8,8 @@
   const mongoose = require('mongoose')
   const session = require("express-session")
   const flash = require("connect-flash")
+  require("./models/Postagem")
+  const Postagem = mongoose.model("postagen")
 
 
 // Configurações
@@ -53,7 +55,16 @@
 // Rotas
 
       app.get("/", (req, res) => {
-        res.render('index')
+        Postagem.find().populate("categoria").sort({data: "desc"}).then((postagens) => {
+          res.render('index', {postagens: postagens})
+        }).catch((err) => {
+          req.flash("error_msg", "Houve um erro")
+          res.redirect("/404")
+        })
+      })
+
+      app.get("/404", (req, res) => {
+        res.send("Erro 404")
       })
 
       app.get('/posts', (req, res) => {
