2053077607ac9d1c6b3485fd4e3535ddc05aeffa
davi <davijluna@gmail.com>
2023-10-06 15:33:05 -0300

    feat: adicionando rota para deletar postagens

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -197,4 +197,15 @@ router.post("/postagem/edit", (req, res) => {
   })
 })
 
+
+router.get("/postagens/deletar/:id", (req, res) => {
+  Postagem.deleteOne({_id:req.params.id}).then(() => {
+    req.flash("success_msg", "postagem deletada com sucesso")
+    res.redirect("/admin/postagens")
+  }).catch((err) => {
+    req.flash("error_msg", "Houve um erro ao deletar postagens"+err)
+    res.redirect("/admin/postagens")
+  })
+})
+
 module.exports = router
